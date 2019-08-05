31e5e48a730880c325058b09496cb368cdf0d64f
Ryan Miller <ryanmillergm@gmail.com>
2019-08-05 11:01:28 -0600

    Adds update route

--- routes/api/v1/games.js
+++ routes/api/v1/games.js
@@ -47,4 +47,29 @@ router.post("/", function(req, res, next) {
     });
 });
 
+router.put("/:id", function(req, res, next) {
+  Game.update(
+    {
+          title: req.body.title,
+          price: req.body.price,
+          releaseYear: req.body.releaseYear,
+          active: req.body.active
+  },
+  {
+    returning: true,
+    where: {
+      id: parseInt(req.params.id)
+    }
+  }
+)
+  .then(([rowsUpdate, [updatedGame]])=> {
+    res.setHeader("Content-Type", "application/json");
+    res.status(202).send(JSON.stringify(updated));
+  })
+  .catch(error => {
+    res.setHeader("Content-Type", "application/json");
+    res.status(500).send({error});
+  });
+});
+
 module.exports = router;
